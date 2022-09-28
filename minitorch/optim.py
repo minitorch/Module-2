ce4e343751630096ca85ef979b4a67f88e83a766
Sasha Rush <srush.research@gmail.com>
2022-09-28 15:30:42 -0400

    Update optim.py

--- minitorch/optim.py
+++ minitorch/optim.py
@@ -1,7 +1,6 @@
 from typing import Sequence
 
 from .module import Parameter
-from .scalar import Scalar
 
 
 class Optimizer:
@@ -29,9 +28,6 @@ class SGD(Optimizer):
         for p in self.parameters:
             if p.value is None:
                 continue
-            if hasattr(p.value, "derivative"):
-                if p.value.derivative is not None:
-                    p.update(Scalar(p.value.data - self.lr * p.value.derivative))
             elif hasattr(p.value, "grad"):
                 if p.value.grad is not None:
                     p.update(p.value - self.lr * p.value.grad)
