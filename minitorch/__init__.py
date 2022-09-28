2fd8e571dd7a678f98d862a6e3fa77cfca7a1ca2
Sasha Rush <srush.research@gmail.com>
2022-09-28 15:30:13 -0400

    Update __init__.py

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -8,5 +8,4 @@ from .optim import *  # noqa: F401,F403
 from .testing import *  # noqa: F401,F403
 from .module import *  # noqa: F401,F403
 from .autodiff import *  # noqa: F401,F403
-from .scalar import *  # noqa: F401,F403
 from .module import *  # noqa: F401,F403
